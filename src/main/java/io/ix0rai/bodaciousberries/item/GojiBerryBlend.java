7aea7fc8671ec7af7630b7f10e8e2170145613a5
ix0rai <ix0rai64@gmail.com>
2022-04-01 20:05:23 -0500

    fix cme and bump to 1.3.3

--- src/main/java/io/ix0rai/bodaciousberries/item/GojiBerryBlend.java
+++ src/main/java/io/ix0rai/bodaciousberries/item/GojiBerryBlend.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import java.util.Iterator;
+
 public class GojiBerryBlend extends Juice {
     public GojiBerryBlend(Settings settings) {
         super(settings);
@@ -12,7 +14,11 @@ public class GojiBerryBlend extends Juice {
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        for (StatusEffectInstance instance : user.getStatusEffects()) {
+        //note: a mod conflict causes a cme here, so we use an iterator over an enhanced for
+        Iterator<StatusEffectInstance> iterator = user.getStatusEffects().iterator();
+
+        while (iterator.hasNext()) {
+            StatusEffectInstance instance = iterator.next();
             user.removeStatusEffect(instance.getEffectType());
         }
 
