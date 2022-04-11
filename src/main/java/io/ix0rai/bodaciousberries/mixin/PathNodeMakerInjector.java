1959b65164de6ef6eea9cd02c85a3d9d35c44e21
ix0rai <ix0rai64@gmail.com>
2022-04-11 17:18:34 -0500

    remove unnecessary check in PathNodeMakerInjector

--- src/main/java/io/ix0rai/bodaciousberries/mixin/PathNodeMakerInjector.java
+++ src/main/java/io/ix0rai/bodaciousberries/mixin/PathNodeMakerInjector.java
@@ -1,7 +1,6 @@
 package io.ix0rai.bodaciousberries.mixin;
 
 import io.ix0rai.bodaciousberries.block.BerryBush;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.util.math.BlockPos;
@@ -15,12 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PathNodeMakerInjector {
     @Inject(method="getCommonNodeType", at = @At("HEAD"), cancellable = true)
     private static void getCommonNodeType(BlockView blockView, BlockPos blockPos, CallbackInfoReturnable<PathNodeType> callbackInfoReturnable) {
-        //this method is what determines if a block is dangerous and is used by the sweet berry bush, so we need to inject here to make our bushes a danger
-        //honestly, I have no idea how path nodes work, so we're checking the higher state too, just to be safe
-        BlockState state = blockView.getBlockState(blockPos.up());
-        BlockState state2 = blockView.getBlockState(blockPos);
-
-        if (state.getBlock() instanceof BerryBush || state2.getBlock() instanceof BerryBush) {
+        if (blockView.getBlockState(blockPos).getBlock() instanceof BerryBush) {
             callbackInfoReturnable.setReturnValue(PathNodeType.DAMAGE_OTHER);
         }
     }
