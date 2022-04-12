cd01f28d4b7992c739571a45b51f283cdd44f051
ix0rai <ix0rai64@gmail.com>
2022-04-11 19:06:20 -0500

    refactor compat for real this time

--- src/main/java/io/ix0rai/bodaciousberries/compat/Compat.java
+++ src/main/java/io/ix0rai/bodaciousberries/compat/Compat.java
@@ -1,13 +1,44 @@
 package io.ix0rai.bodaciousberries.compat;
 
 import io.ix0rai.bodaciousberries.Bodaciousberries;
+import io.ix0rai.bodaciousberries.block.entity.JuicerRecipes;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Compat {
     public static void registerCompat() {
-        String s = MoreBerries.init() +
-                ImprovedBerries.init();
-        if (!s.isEmpty()) {
-            Bodaciousberries.LOGGER.info("compat loaded with mods: " + s.replace(',', ' ').stripTrailing());
+        final List<String> loadedCompat = init();
+
+        if (!loadedCompat.isEmpty()) {
+            Bodaciousberries.LOGGER.info("compat loaded with mods: " + loadedCompat);
         }
+    }
+    
+    public static List<String> init() {
+        final List<String> loadedCompat = new ArrayList<>();
+
+        String id = "moreberries";
+        if (FabricLoaderImpl.INSTANCE.isModLoaded(id)) {
+            List<String> berryIds = List.of("purple", "yellow", "green", "black", "orange", "blue");
+
+            for (String berryId : berryIds) {
+                JuicerRecipes.addJuiceRecipe(new Identifier(id, berryId + "_berries"), new Identifier(id, berryId + "_berry_juice"));
+            }
+
+            loadedCompat.add(id);
+        }
+
+        id = "improved_berries";
+        if (FabricLoaderImpl.INSTANCE.isModLoaded(id)) {
+            JuicerRecipes.addJuiceRecipe(Registry.ITEM.getId(Items.SWEET_BERRIES), Registry.ITEM.getId(Items.SWEET_BERRIES), Registry.ITEM.getId(Items.SUGAR), new Identifier(id, "sweet_berry_wine"));
+            loadedCompat.add(id);
+        }
+
+        return loadedCompat;
     }
 }
