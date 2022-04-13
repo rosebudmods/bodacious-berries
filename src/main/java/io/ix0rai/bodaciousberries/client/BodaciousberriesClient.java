36726e4f5ed88141feb0d7fc6315813b1546a5f7
ix0rai <ix0rai64@gmail.com>
2022-04-12 21:00:02 -0500

    update for fabric api 0.50

--- src/main/java/io/ix0rai/bodaciousberries/client/BodaciousberriesClient.java
+++ src/main/java/io/ix0rai/bodaciousberries/client/BodaciousberriesClient.java
@@ -9,15 +9,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class BodaciousberriesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ScreenRegistry.register(BodaciousBlocks.BERRY_HARVESTER_SCREEN_HANDLER, BerryHarvesterScreen::new);
-        ScreenRegistry.register(BodaciousBlocks.JUICER_SCREEN_HANDLER, JuicerScreen::new);
+        HandledScreens.register(BodaciousBlocks.BERRY_HARVESTER_SCREEN_HANDLER, BerryHarvesterScreen::new);
+        HandledScreens.register(BodaciousBlocks.JUICER_SCREEN_HANDLER, JuicerScreen::new);
 
         for (var entry : Berries.BERRY_BUSHES.entrySet()) {
             //ensure bush is rendered with a cutout
--- src/main/java/io/ix0rai/bodaciousberries/registry/BodaciousBlocks.java
+++ src/main/java/io/ix0rai/bodaciousberries/registry/BodaciousBlocks.java
@@ -9,7 +9,6 @@ import io.ix0rai.bodaciousberries.block.entity.JuicerBlockEntity;
 import io.ix0rai.bodaciousberries.block.entity.JuicerScreenHandler;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
@@ -24,24 +23,23 @@ public class BodaciousBlocks {
     public static final Identifier BERRY_HARVESTER = Bodaciousberries.id("berry_harvester");
     public static final BerryHarvesterBlock BERRY_HARVESTER_BLOCK = new BerryHarvesterBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
     public static final BlockEntityType<BerryHarvesterBlockEntity> BERRY_HARVESTER_ENTITY = FabricBlockEntityTypeBuilder.create(BerryHarvesterBlockEntity::new, BERRY_HARVESTER_BLOCK).build(null);
-    public static ScreenHandlerType<BerryHarvesterScreenHandler> BERRY_HARVESTER_SCREEN_HANDLER;
+    public static final ScreenHandlerType<BerryHarvesterScreenHandler> BERRY_HARVESTER_SCREEN_HANDLER = new ScreenHandlerType<>(BerryHarvesterScreenHandler::new);
 
     public static final Identifier JUICER = Bodaciousberries.id("juicer");
     public static final JuicerBlock JUICER_BLOCK = new JuicerBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
     public static final BlockEntityType<JuicerBlockEntity> JUICER_ENTITY = FabricBlockEntityTypeBuilder.create(JuicerBlockEntity::new, JUICER_BLOCK).build(null);
-    public static ScreenHandlerType<JuicerScreenHandler> JUICER_SCREEN_HANDLER;
+    public static final ScreenHandlerType<JuicerScreenHandler> JUICER_SCREEN_HANDLER = new ScreenHandlerType<>(JuicerScreenHandler::new);
 
     public static void registerBlocks() {
-        BERRY_HARVESTER_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(BERRY_HARVESTER, BerryHarvesterScreenHandler::new);
-        registerBlockEntity(BERRY_HARVESTER, BERRY_HARVESTER_BLOCK, BERRY_HARVESTER_ENTITY, ItemGroup.REDSTONE);
+        registerBlockEntity(BERRY_HARVESTER, BERRY_HARVESTER_BLOCK, BERRY_HARVESTER_ENTITY, BERRY_HARVESTER_SCREEN_HANDLER, ItemGroup.REDSTONE);
 
-        JUICER_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(JUICER, JuicerScreenHandler::new);
-        registerBlockEntity(JUICER, JUICER_BLOCK, JUICER_ENTITY, ItemGroup.BREWING);
+        registerBlockEntity(JUICER, JUICER_BLOCK, JUICER_ENTITY, JUICER_SCREEN_HANDLER, ItemGroup.BREWING);
     }
 
-    private static void registerBlockEntity(Identifier id, Block block, BlockEntityType<?> entity, ItemGroup group) {
+    private static void registerBlockEntity(Identifier id, Block block, BlockEntityType<?> entity, ScreenHandlerType<?> handler, ItemGroup group) {
         Registry.register(Registry.BLOCK, id, block);
         Registry.register(Registry.ITEM, id, new BlockItem(block, new Settings().group(group)));
+        Registry.register(Registry.SCREEN_HANDLER, id, handler);
         Registry.register(Registry.BLOCK_ENTITY_TYPE, Bodaciousberries.id(id.getPath() + "_entity"), entity);
     }
 }
