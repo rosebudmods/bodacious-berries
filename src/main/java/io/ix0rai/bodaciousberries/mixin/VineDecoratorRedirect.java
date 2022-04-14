e4b1e847df4ead042e03a59722cebcbddb6d6b0c
ix0rai <ix0rai64@gmail.com>
2022-04-14 18:56:20 -0500

    change vine decorator mixin to an injector that only redirects the method if the biome is a jungle

--- src/main/java/io/ix0rai/bodaciousberries/mixin/VineDecoratorRedirect.java
+++ src/main/java/io/ix0rai/bodaciousberries/mixin/VineDecoratorRedirect.java
@@ -13,28 +13,35 @@ import net.minecraft.world.TestableWorld;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.treedecorator.LeavesVineTreeDecorator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BiConsumer;
 
 @Mixin(LeavesVineTreeDecorator.class)
-public class VineDecoratorOverwrite {
+public class VineDecoratorRedirect {
     /**
      * @reason get grapevines to spawn on trees more naturally
      * @author ix0rai
      */
-    @Overwrite
-    private static void placeVines(TestableWorld world, BlockPos pos, BooleanProperty facing, BiConsumer<BlockPos, BlockState> replacer) {
+    @Inject(method = "placeVines", at = @At("HEAD"), cancellable = true)
+    private static void placeVines(TestableWorld world, BlockPos pos, BooleanProperty facing, BiConsumer<BlockPos, BlockState> replacer, CallbackInfo ci) {
         //convert world to structure world access so that we can test for vines and air blocks
         final StructureWorldAccess access = (StructureWorldAccess) world;
 
-        placeVine(access, replacer, pos, facing);
-
-        //place vines that are hanging down from other vines
-        int i = access.getRandom().nextInt(3, 6);
-        for(pos = pos.down(); Feature.isAir(world, pos) && i > 0; --i) {
+        //only redirect the method if we're in a jungle biome
+        if (access.getBiome(pos).isIn(BiomeTags.IS_JUNGLE)) {
             placeVine(access, replacer, pos, facing);
-            pos = pos.down();
+
+            //place vines that are hanging down from other vines
+            int i = access.getRandom().nextInt(3, 6);
+            for(pos = pos.down(); Feature.isAir(world, pos) && i > 0; --i) {
+                placeVine(access, replacer, pos, facing);
+                pos = pos.down();
+            }
+
+            ci.cancel();
         }
     }
 
@@ -46,7 +53,7 @@ public class VineDecoratorOverwrite {
             block = Blocks.VINE.getDefaultState().with(facing, true);
         } else if (access.getBlockState(pos.up()).getBlock() == Bushes.GRAPEVINE){
             block = Bushes.GRAPEVINE.getDefaultState().with(facing, true).with(BerryVine.AGE, 3);
-        } else if (reallyIncrediblyStupidAwfulHorrendousDumbCheck(access, pos)) {
+        } else if (reallyIncrediblyStupidAwfulHorrendousTerribleHorribleDumbCheck(access, pos)) {
             //otherwise, if reallyIncrediblyStupidAwfulHorrendousDumbCheck confirms that we won't be placing a floating vine, choose a vine or grapevine
             if (access.getBiome(pos).isIn(BiomeTags.IS_JUNGLE) && access.getRandom().nextInt(6) == 0) {
                 block = Bushes.GRAPEVINE.getDefaultState().with(facing, true).with(BerryVine.AGE, 3);
@@ -63,7 +70,7 @@ public class VineDecoratorOverwrite {
         replacer.accept(pos, block);
     }
 
-    private static boolean reallyIncrediblyStupidAwfulHorrendousDumbCheck(StructureWorldAccess access, BlockPos pos) {
+    private static boolean reallyIncrediblyStupidAwfulHorrendousTerribleHorribleDumbCheck(StructureWorldAccess access, BlockPos pos) {
         final Block east = access.getBlockState(pos.east()).getBlock();
         final Block west = access.getBlockState(pos.west()).getBlock();
         final Block north = access.getBlockState(pos.north()).getBlock();
