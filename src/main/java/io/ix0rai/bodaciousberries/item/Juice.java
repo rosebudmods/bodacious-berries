ab40ee9d0b6c9b4fe4ec2c15ffc71da0e3bf14ab
ix0rai <ix0rai64@gmail.com>
2022-05-14 16:17:31 -0500

    cleanup: make Juice extend HoneyBottleItem

--- src/main/java/io/ix0rai/bodaciousberries/item/Juice.java
+++ src/main/java/io/ix0rai/bodaciousberries/item/Juice.java
@@ -5,20 +5,15 @@ import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.HoneyBottleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 import java.security.InvalidParameterException;
 
-public class Juice extends Item {
+public class Juice extends HoneyBottleItem {
     private final Item berry;
 
     public Juice(Item berry) {
@@ -76,31 +71,6 @@ public class Juice extends Item {
 
             return stack;
         }
-    }
-
-    @Override
-    public int getMaxUseTime(ItemStack stack) {
-        return 40;
-    }
-
-    @Override
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.DRINK;
-    }
-
-    @Override
-    public SoundEvent getDrinkSound() {
-        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
-    }
-
-    @Override
-    public SoundEvent getEatSound() {
-        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return ItemUsage.consumeHeldItem(world, user, hand);
     }
 
     public Item getBerry() {
