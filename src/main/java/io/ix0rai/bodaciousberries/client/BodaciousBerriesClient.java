d570643787013b56a5bee30338c162db19f094a4
ix0rai <ix0rai64@gmail.com>
2022-07-30 18:54:06 -0500

    apply colour providers to all bushes (textures still todo)

--- src/main/java/io/ix0rai/bodaciousberries/client/BodaciousBerriesClient.java
+++ src/main/java/io/ix0rai/bodaciousberries/client/BodaciousBerriesClient.java
@@ -1,5 +1,6 @@
 package io.ix0rai.bodaciousberries.client;
 
+import io.ix0rai.bodaciousberries.block.BerryBush;
 import io.ix0rai.bodaciousberries.block.entity.BerryHarvesterScreen;
 import io.ix0rai.bodaciousberries.block.entity.JuicerScreen;
 import io.ix0rai.bodaciousberries.client.particle.Particles;
@@ -9,9 +10,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.FoliageColors;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.item.Item;
+
+import java.util.Map;
 
 @Environment(EnvType.CLIENT)
 public class BodaciousBerriesClient implements ClientModInitializer {
@@ -20,9 +27,17 @@ public class BodaciousBerriesClient implements ClientModInitializer {
         HandledScreens.register(BodaciousBlocks.BERRY_HARVESTER_SCREEN_HANDLER, BerryHarvesterScreen::new);
         HandledScreens.register(BodaciousBlocks.JUICER_SCREEN_HANDLER, JuicerScreen::new);
 
-        for (var entry : Berries.BERRY_BUSHES.entrySet()) {
+        for (Map.Entry<BerryBush, Item> entry : Berries.BERRY_BUSHES.entrySet()) {
             // ensure bush is rendered with a cutout
-            BlockRenderLayerMap.INSTANCE.putBlock(((Block) entry.getKey()), RenderLayer.getCutout());
+            Block block = (Block) entry.getKey();
+            BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
+            ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> {
+                if (view != null) {
+                    return BiomeColors.getFoliageColor(view, pos);
+                } else {
+                    return FoliageColors.getDefaultColor();
+                }
+            }, block);
         }
 
         Particles.registerParticles();
