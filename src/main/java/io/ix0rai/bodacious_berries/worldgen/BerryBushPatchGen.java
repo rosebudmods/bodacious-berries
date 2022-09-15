38b03fbb7de2b95a303d6760d7ed40ecf4cb7632
ix0rai <ix0rai64@gmail.com>
2022-09-14 20:06:00 -0500

    add individual config options to generate each bush

--- src/main/java/io/ix0rai/bodacious_berries/worldgen/BerryBushPatchGen.java
+++ src/main/java/io/ix0rai/bodacious_berries/worldgen/BerryBushPatchGen.java
@@ -174,7 +174,7 @@ public class BerryBushPatchGen {
                 context -> {
                     Holder<Biome> entry = context.getBiomeRegistryEntry();
                     for (TagKey<Biome> tag : tags) {
-                        if (entry.isIn(tag)) {
+                        if (entry.hasTag(tag)) {
                             return true;
                         }
                     }
