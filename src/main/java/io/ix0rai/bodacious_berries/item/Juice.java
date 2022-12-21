d4eb1cbc93a3c20b9f5dd933216d3928a66a7a47
ix0rai <ix0rai64@gmail.com>
2022-12-20 20:05:30 -0600

    fix broken glass bottle logic

--- src/main/java/io/ix0rai/bodacious_berries/item/Juice.java
+++ src/main/java/io/ix0rai/bodacious_berries/item/Juice.java
@@ -1,15 +1,11 @@
 package io.ix0rai.bodacious_berries.item;
 
 import io.ix0rai.bodacious_berries.registry.BodaciousJuices;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.HoneyBottleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
@@ -55,43 +51,9 @@ public class Juice extends HoneyBottleItem {
         throw new InvalidParameterException("item: " + berry + " does not have a food component");
     }
 
-    @Override
-    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        // we handle changing the stack
-        ItemStack stack1 = stack.copy();
-        super.finishUsing(stack, world, user);
-        stack = stack1;
-
-        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
-            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
-        }
-
-        // return empty bottle, or throw it away if it does not fit
-        // also decrement the stack size if it will not be entirely consumed
-        if (stack.isEmpty()) {
-            return new ItemStack(BodaciousJuices.JUICE_RECEPTACLE);
-        } else {
-            if (user instanceof PlayerEntity playerEntity && !playerEntity.getAbilities().creativeMode) {
-                ItemStack glassBottle = new ItemStack(BodaciousJuices.JUICE_RECEPTACLE);
-                if (!playerEntity.getInventory().insertStack(glassBottle)) {
-                    playerEntity.dropItem(glassBottle, false);
-                }
-
-                if (stack.getCount() == 1) {
-                    return glassBottle;
-                } else {
-                    stack.decrement(1);
-                    return stack;
-                }
-            }
-
-            return stack;
-        }
-    }
-
     public Item getBerry() {
         if (berry == null) {
-            throw new IllegalStateException("this juice is a berry blend. shut up.");
+            throw new IllegalStateException("Illegal getBerry() call on a berry blend juice item! Please report this.");
         }
         return berry;
     }
